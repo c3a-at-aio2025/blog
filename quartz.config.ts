67056648255b316b417860ffb6fa69ea26d1e7db
Vo Ta Quang Nhat <votaquangnhat1104@gmail.com>
2025-10-05 21:33:48 +0700

    Quartz sync: Oct 5, 2025, 9:33 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "C3A@AIO2025",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
